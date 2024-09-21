b4df2d979d905eccafb3f33fd941f391c584f5ce
PatrickCoop12 <123266129+PatrickCoop12@users.noreply.github.com>
2024-09-21 03:17:21 -0700

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,5 @@ import {Inter} from 'next/font/google'
 import {Lusitana} from 'next/font/google'
 import {Roboto} from 'next/font/google'
 export const inter = Inter({subsets: ['latin']})
-export const lusitana : Lusitana({subsets:['latin'], weight: "400"})
-export const roboto : Roboto({subsets:['cyrillic'], weight: "900"})
+export const lusitana = Lusitana({subsets:['latin'], weight: "400"})
+export const roboto = Roboto({subsets:['cyrillic'], weight: "900"})
